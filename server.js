af2e18a49a8925664624e44ec97b90622467bd25
Yash Bharatula <ybharatu@purdue.edu>
2019-08-12 23:17:55 -0400

    Heroku Debug

--- server.js
+++ server.js
@@ -1,14 +1,28 @@
 const express = require('express')
 const app = express()
 
+const http = require('http');
+const port = process.env.PORT || 3000
+
 app.use(express.static(__dirname + '/assets'));
 app.set('view engine', 'ejs')
 
-app.get('/', function (req, res) {
-	// res.send('Hello World!')
-	res.render('index');
-})
+// app.get('/', function (req, res) {
+// 	// res.send('Hello World!')
+// 	res.render('index');
+// })
 
-app.listen(3000, function () {
-	console.log('Example app listening on port 3000!')
-})
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.render('index')
+  // res.setHeader('Content-Type', 'text/html');
+  // res.end('<h1>Hello World</h1>');
+});
+
+server.listen(port,() => {
+  console.log(`Server running at port `+port);
+});
+
+// app.listen(3000, function () {
+// 	console.log('Example app listening on port 3000!')
+// })
