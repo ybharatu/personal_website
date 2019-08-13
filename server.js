27f233ba63e033f294be9ad7030583eda4a77a4b
Yash Bharatula <ybharatu@purdue.edu>
2019-08-13 01:22:50 -0400

    SSL Debug

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const port = process.env.PORT || 3000
 app.use(express.static(__dirname + '/assets'));
 app.set('view engine', 'ejs')
 
-app.get('/.well-known/acme-challenge/FSAUh04H3iXSqjHXXfvgQZ7y8k9U5Pz3giZYuDLxDPo', function(req, res) {
-  res.send('FSAUh04H3iXSqjHXXfvgQZ7y8k9U5Pz3giZYuDLxDPo.96BF3Cazuj4DF3MFKt4XU2VuOuOzqP0P0rX6JWtoEYg')
+app.get('/.well-known/acme-challenge/jmxZiVpCcVCytSRgjtrO6JxTRetudYM9dj1ZxgFXG3M', function(req, res) {
+  res.send('jmxZiVpCcVCytSRgjtrO6JxTRetudYM9dj1ZxgFXG3M.96BF3Cazuj4DF3MFKt4XU2VuOuOzqP0P0rX6JWtoEYg')
 })
 
 app.get('/', function (req, res) {
