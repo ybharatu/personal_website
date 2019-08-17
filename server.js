b7f4f49403e5833776154b54a4cbedd065edb02e
Yash Bharatula <ybharatu@purdue.edu>
2019-08-17 12:07:22 -0400

    Email Form not working

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express')
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
 const app = express()
 
 const port = process.env.PORT || 3000
 
 app.use(express.static(__dirname + '/assets'));
 app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 app.get('/.well-known/acme-challenge/Tu-OKRCGPX9YlzcfKKQRhFMr68hJn08GNgO12uq-7_o', function(req, res) {
   res.send('Tu-OKRCGPX9YlzcfKKQRhFMr68hJn08GNgO12uq-7_o.96BF3Cazuj4DF3MFKt4XU2VuOuOzqP0P0rX6JWtoEYg')
@@ -30,6 +36,36 @@ app.get('/amd_page', function (req, res) {
 	// res.send('Hello World!')
 	res.render('amd_page');
 })
+
+// POST route from contact form
+// app.post('/contact', function (req, res) {
+//   let mailOpts, smtpTrans;
+//   smtpTrans = nodemailer.createTransport({
+//     host: 'smtp.gmail.com',
+//     port: 465,
+//     secure: true,
+//     auth: {
+//       user: yashwanthb,
+//       pass: syiaripdkuwyqohc
+//     }
+//   });
+//   mailOpts = {
+//     from: req.body.name + ' &lt;' + req.body.email + '&gt;',
+//     to: yashwanthb,
+//     subject: 'New message from contact form at tylerkrys.ca',
+//     text: `${req.body.name} (${req.body.email}) says: ${req.body.message}`
+//   };
+//   smtpTrans.sendMail(mailOpts, function (error, response) {
+//     if (error) {
+//       // res.render('contact-failure');
+//       res.render('failure')
+//     }
+//     else {
+//       // res.render('contact-success');
+//       res.render('index')
+//     }
+//   });
+// });
 
 app.listen(port, function () {
 	console.log('Website app listening on port 3000!')
